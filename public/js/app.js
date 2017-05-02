68ae34addba9485a1c28d54b078fe0014f7fb939
Jenn Spencer <jennspencer@gmail.com>
2017-05-02 12:27:29 -0700

    adding in rudimentary search function

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,19 @@
 (function(){
 	var app = angular.module('libraryFetcherApp', []);
+		
+	var projects;
+
+	app.controller('ProjectController', function($scope, $http) {
+        
+        $scope.searchProjects = function() {
+            $scope.url = 'https://libraries.io/api/search?q=' + $scope.searchString + '&api_key=7fb8f1e564967350609950bb3d913b4a';
+		    $http.get($scope.url).then(function(response) {
+        	    projects = response.data;
+        	    $scope.projects = projects;
+    	    }).catch(function(error){
+    		    console.log(error);
+    	    });
+	    };
+    }); 
 
 })();
