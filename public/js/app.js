f3390739a992c4a524e2281044020d81a2a7fc84
Jenn Spencer <jennspencer@gmail.com>
2017-05-02 17:43:33 -0700

    filtering projects on NPM platform

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,33 @@
 (function(){
-	var app = angular.module('libraryFetcherApp', []);
-		
-	var projects;
+    var app = angular.module('libraryFetcherApp', []);
 
-	app.controller('ProjectController', function($scope, $http) {
-        
+    var projects;
+
+    app.controller('ProjectController', function($scope, $http) {
+
         $scope.searchProjects = function() {
-            $scope.url = 'https://libraries.io/api/search?q=' + $scope.searchString + '&api_key=7fb8f1e564967350609950bb3d913b4a';
-		    $http.get($scope.url).then(function(response) {
-        	    projects = response.data;
-        	    $scope.projects = projects;
-                
-    	    }).catch(function(error){
-    		    console.log(error);
-    	    });
-	    };
-        
+            
+            url = 'https://libraries.io/api/search?q=' + $scope.searchString + '&api_key=7fb8f1e564967350609950bb3d913b4a';
+            
+            if (!!$scope.searchString) {
+                $http.get(url).then(function(response) {
+                    projects = response.data;
+                    projects = _.filter(projects, { platform: 'NPM' });
+                    _.forEach(projects, function(project) {
+                        if (project.versions.length === 0) {
+                            project.versions.push({
+                                number: project.latest_release_number,
+                                published_at: project.latest_release_published_at,
+                            });
+                        }
+                    });
+                    $scope.projects = projects;
+                }).catch(function(error){
+                    console.log(error);
+                });
+            }
+        };
+
         $scope.versionLimit = 5;
 
     }); 
