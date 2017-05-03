dd0b78d13463dd3203f0b7a54c84925895da848e
Jenn Spencer <jennspencer@gmail.com>
2017-05-02 18:26:16 -0700

    make pretty and more functional
    error messaging, spinner, better search functionality

--- public/js/app.js
+++ public/js/app.js
@@ -6,12 +6,15 @@
     app.controller('ProjectController', function($scope, $http) {
 
         $scope.searchProjects = function() {
-            
+            $scope.projects = [];
             url = 'https://libraries.io/api/search?q=' + $scope.searchString + '&api_key=7fb8f1e564967350609950bb3d913b4a';
-            
+            $scope.errorMessage = false;
+
             if (!!$scope.searchString) {
+                $scope.thinking = true;
                 $http.get(url).then(function(response) {
                     projects = response.data;
+                    if (projects.length === 0) $scope.errorMessage = true;
                     projects = _.filter(projects, { platform: 'NPM' });
                     _.forEach(projects, function(project) {
                         if (project.versions.length === 0) {
@@ -22,6 +25,7 @@
                         }
                     });
                     $scope.projects = projects;
+                    $scope.thinking = false;
                 }).catch(function(error){
                     console.log(error);
                 });
